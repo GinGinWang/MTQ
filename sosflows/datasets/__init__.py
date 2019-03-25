035cc17491f50f2038b9e188b24dcc135f7904b0
GinGinWang <48892043+GinGinWang@users.noreply.github.com>
2019-03-25 15:42:30 -0400

    Add files via upload

---
+++ sosflows/datasets/__init__.py
@@ -0,0 +1,9 @@
+root = 'data/'
+
+from datasets.power import POWER
+from datasets.gas import GAS
+from datasets.hepmass import HEPMASS
+from datasets.miniboone import MINIBOONE
+from datasets.bsds300 import BSDS300
+from datasets.mnist import MNIST
+from datasets.cifar10 import CIFAR10
