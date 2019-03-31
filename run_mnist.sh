878b7d81b2648159f5a880bb6633999698bdde70
GinGinWang <jj27wang@uwaterloo.ca>
2019-03-31 14:44:42 -0400

    experiment

--- run_mnist.sh
+++ run_mnist.sh
@@ -4,18 +4,18 @@ echo "START OUR Expermients!"
 
 echo "MAF-LSA MINIST"
 
-echo "Train"
-python train.py --autoencoder LSA --estimator MAF --dataset mnist --batch_size 1000 --lr 0.001 --num_blocks 5  --epochs 1000 --combine_density True 
+# echo "Train"
+# python train.py --autoencoder LSA --estimator MAF --dataset mnist --batch_size 1000 --lr 0.001 --num_blocks 5  --epochs 1000 --combine_density True 
 
-echo "Train"
-python train.py --autoencoder LSA --estimator MAF --dataset mnist --batch_size 1000 --lr 0.001 --num_blocks 5  --epochs 1000 --combine_density False
+# echo "Train"
+# python train.py --autoencoder LSA --estimator MAF --dataset mnist --batch_size 1000 --lr 0.001 --num_blocks 5  --epochs 1000 --combine_density False
 
 echo "Test score_normed combine_density"
 # novel from 0.1 to 0.5
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num-blocks 5 --combine_density True --score_normed True  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num_blocks 5 --combine_density True --score_normed True  --novel_ratio $i
 	done
 
 
@@ -23,7 +23,7 @@ echo "Test not score_normed, combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num-blocks 5 --combine_density True --score_normed False  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num_blocks 5 --combine_density True --score_normed False  --novel_ratio $i
 	done
 
 
@@ -31,7 +31,7 @@ echo "Test score_normed, not combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num-blocks 5 --combine_density False --score_normed True  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num_blocks 5 --combine_density False --score_normed True  --novel_ratio $i
 	done
 
 
@@ -40,7 +40,7 @@ echo "Test not score_normed, not combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num-blocks 5 --combine_density False --score_normed False  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator MAF --dataset mnist --num_blocks 5 --combine_density False --score_normed False  --novel_ratio $i
 	done
 
 
@@ -55,7 +55,7 @@ echo "Test score_normed combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num-blocks 5 --combine_density True --score_normed True  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num_blocks 5 --combine_density True --score_normed True  --novel_ratio $i
 	done
 
 
@@ -63,7 +63,7 @@ echo "Test not score_normed, combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num-blocks 5 --combine_density True --score_normed False  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num_blocks 5 --combine_density True --score_normed False  --novel_ratio $i
 	done
 
 
@@ -71,7 +71,7 @@ echo "Test score_normed, not combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num-blocks 5 --combine_density False --score_normed True  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num_blocks 5 --combine_density False --score_normed True  --novel_ratio $i
 	done
 
 
@@ -80,5 +80,5 @@ echo "Test not score_normed, not combine_density"
 for i in  0.1 0.2 0.3 0.4 0.5
 	do 
 		echo "Test on novel_ratio=$i" 
-		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num-blocks 5 --combine_density False --score_normed False  --novel_ratio $i
+		python test.py --autoencoder LSA --estimator SOS --dataset mnist --num_blocks 5 --combine_density False --score_normed False  --novel_ratio $i
 	done
