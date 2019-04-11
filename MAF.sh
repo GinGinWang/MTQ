0079f7808944e3e939470e635d11108e85396078
GinGinWang <jj27wang@uwaterloo.ca>
2019-04-11 00:50:21 -0400

    test

--- MAF.sh
+++
@@ -1,7 +0,0 @@
-echo "START OUR Expermients!"
-
-echo "MAF MINIST"
-
-# Not combine density#
- echo "Train"
-python train.py --estimator MAF --dataset mnist --batch_size 1000  --num_blocks 5 --epochs 10 
